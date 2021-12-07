6996275fde64333f9bb7bfa66e122efe03552ac3
Raj-04 <1204patel.raj@gmail.com>
2021-12-07 17:40:46 -0500

    checkCarId middleware done

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,8 +1,21 @@
 const Car = require('./cars-model')
 const vin = require('vin-validator')
 
-const checkCarId = (req, res, next) => {
-  
+const checkCarId = async (req, res, next) => {
+  try {
+    const car = await Car.getById(req.params.id)
+    if(!car) {
+      next({
+        status: 404,
+        message: 'not found'
+      })
+    } else {
+      req.car = car
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
