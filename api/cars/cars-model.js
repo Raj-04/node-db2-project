fc4e0fd8809fb0681ad99781ff826ffebdc1eba8
Raj-04 <1204patel.raj@gmail.com>
2021-12-07 17:34:05 -0500

    created the model for the cars

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,19 +1,32 @@
 const db = require('../../data/db-config')
 
 const getAll = () => {
-  
+  return db('cars')
 }
 
 const getById = () => {
-  
+  return db('cars')
+    .where('id', id)
+    .first()
 }
 
-const create = () => {
-  
+const getByVin = (vin) => {
+  return db('cars')
+    .where('vin', vin)
+    .first()
+}
+
+const create = (car) => {
+  return db('cars')
+    .insert(car)
+    .then((id) => {
+      return getByVin(id)
+    })
 }
 
 module.exports = {
   getAll,
   getById,
   create,
+  getByVin,
 }
